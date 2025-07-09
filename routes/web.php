d6763df209e69addd3cd1cafb65f9529e2f82e4e
Bleedmagic <bleedmagic87@gmail.com>
2025-07-09 11:09:25 +0800

    feat: ✨ add Header and Footer components, implement PublicLayout, and create initial page structures for Contact, Membership, and Services

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,21 @@ Route::get('about', function () {
   return Inertia::render('About');
 })->name('about');
 
+Route::get('contact', function () {
+  return Inertia::render('Contact');
+})->name('contact');
+
+Route::get('Services', function () {
+  return Inertia::render('Services');
+})-> name('services');
+
+Route::get('Membership', function () {
+  return Inertia::render('Membership');
+})->name('membership');
+
 Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+  return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
